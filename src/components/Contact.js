a90b17336c34ff16a86ae539b76824d8a83510b8
med <mouslfg65@gmail.com>
2023-03-09 14:15:13 -0500

    Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,6 @@ import { db } from './firebase'
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 function Contact() {
 
@@ -19,24 +17,34 @@ const [fille,setFille]= useState("");
 const [naissancefille,setNaissanefille]= useState("");
 const [niveauetude,setNiveauetude]= useState("");
 const [sport,setSport]= useState("");
-const [autresport,setAutresport]= useState("");
+const [sport1,setSport1]= useState("");
 const [sport2,setSport2]= useState("");
+const [sport3,setSport3]= useState("");
+const [sport4,setSport4]= useState("");
+const [sport5,setSport5]= useState("");
+const [autresport,setAutresport]= useState("");
 const [autresport2,setAutresport2]= useState("");
-
+const [autresport3,setAutresport3]= useState("");
 const [culture,setCulture]= useState("");
+const [culture2,setCulture2]=useState("");
+const [culture3,setCulture3]=useState("");
+const [culture4,setCulture4]=useState("");
+const [culture5,setCulture5]=useState("");
+const [culture6,setCulture6]=useState("");
 const [autreculture,setAutreculture]=useState("");
+const [autreculture2,setAutreculture2]=useState("");
+const [autreculture3,setAutreculture3]=useState("");
 const [fichier, setFichier]= useState("");
-const [football, setFootball]= useState("");
-const [theatre,setTheatre]= useState("");
-
 
 const handleSubmit =(e)=>{
     e.preventDefault ();
     db.collection("figuig").add({
       Nom,Prenom,Telephone,Adresse,email,garçon,naissancegarçon
-      ,fille,naissancefille,niveauetude,sport,autresport,
-      sport2,autresport2,
-      culture,autreculture,fichier,football,theatre
+      ,fille,naissancefille,niveauetude,sport,sport1,sport2,
+      sport3,sport4,sport5,autresport,
+      autresport2,autresport3,culture,
+      culture2,culture3,culture4,culture5,
+      culture6,autreculture,autreculture2,autreculture3,fichier,
     })
     .then(()=>{alert('Message envoyé');} )
     .catch((error)=>{alert(error.message);});
@@ -52,14 +60,25 @@ const handleSubmit =(e)=>{
     setNaissanefille("");
     setNiveauetude("");
     setSport("");
-    setAutresport("");
+    setSport1("");
     setSport2("");
+    setSport3("");
+    setSport4("");
+    setSport5("");
+    setAutresport("");
     setAutresport2("");
+    setAutresport3("");
     setCulture("");
+    setCulture2("");
+    setCulture3("");
+    setCulture4("");
+    setCulture5("");
+    setCulture6("");
     setAutreculture("");
+    setAutreculture2("");
+    setAutreculture3("");
     setFichier("");
-    setFootball("");
-    setTheatre("");
+    
 };
 
   return (
@@ -135,264 +154,136 @@ as="textarea"></Form.Control>
 
     <div className='sportpreferer'><h5>النوع الرياضي المفضل</h5> </div>
  
+
  <Form.Group className="mb-2 mt-2 w-50 p-2">
  <Form.Control onChange={e=>setSport(e.target.value)} 
  type="text" value={sport} placeholder="اسم الابن او الابنة و عمره (اختيار النوع الرياضي)" />
+ </Form.Group>
+ <Form.Select  className="mb-2 mt-2 w-50 p-2" aria-label="Default select example"
+ onChange={e=>setSport1(e.target.value)}
+value={sport1}>
+ <option>اختر نوع رياضي</option>
+ <option value="كرة القدم">كرة القدم </option>
+ <option value="الكرة الطائرة">الكرة الطائرة</option>
+ <option value="كرة السلة">كرة السلة</option>
+<option value="السباحة">السباحة</option>
+<option value="ألعاب القوى">ألعاب القوى</option>
+<option value="رياضة اخرى"></option>
+</Form.Select>
+
+<Form.Group className="w-50 p-2 mr-3">
+  <Form.Control onChange={e=>setAutresport(e.target.value)} 
+     type="text" value={autresport} placeholder="النوع الرياضي الاخر" />
+</Form.Group>
+
+<Form.Group className="mb-2 mt-2 w-50 p-2">
+<Form.Control onChange={e=>setSport2(e.target.value)} 
+type="text" value={sport2} placeholder="اسم الابن او الابنة و عمره (اختيار النوع الرياضي)" />
+</Form.Group>
+<Form.Select  className="mb-2 mt-2 w-50 p-2" aria-label="Default select example"
+onChange={e=>setSport3(e.target.value)}
+value={sport3}>
+<option>اختر نوع رياضي</option>
+<option value="كرة القدم">كرة القدم </option>
+<option value="الكرة الطائرة">الكرة الطائرة</option>
+<option value="كرة السلة">كرة السلة</option>
+<option value="السباحة">السباحة</option>
+<option value="ألعاب القوى">ألعاب القوى</option>
+<option value="رياضة اخرى"></option>
+</Form.Select>
+
+<Form.Group className="w-50 p-2 mr-3">
+ <Form.Control onChange={e=>setAutresport2(e.target.value)} 
+    type="text" value={autresport2} placeholder="النوع الرياضي الاخر" />
+</Form.Group>
  
-<Form.Check  className="mb-0  "
-type="checkbox" label="كرة القدم" onChange={e=>setFootball(e.target.value)}
-value={football}/>
-<Form.Check  className="mb-0  "
-type="checkbox" label="الكرة الطائرة" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="كرة السلة " />
-<Form.Check  className="mb-0  "
-type="checkbox" label="السباحة " />
-<Form.Check  className="mb-0  "
-type="checkbox" label="ألعاب القوى" />
-</Form.Group>
-
-<Row className="autre">
-  <Col>
-   <Form.Check  className="mb-0  "
-     type="checkbox" label="نوع اخر"/>
-  </Col>
-  <Col sm>
-<Form.Group className="w-50 p-2 mr-3">
-  <Form.Control onChange={e=>setAutresport(e.target.value)} 
-     type="text" value={autresport} placeholder="النوع الرياضي الاخر" />
-</Form.Group>
-  </Col>
-</Row>
-
 <Form.Group className="mb-2 mt-2 w-50 p-2">
- <Form.Control onChange={e=>setSport2(e.target.value)} 
- type="text" value={sport2} placeholder="اسم الابن او الابنة و عمره (اختيار النوع الرياضي)" />
- </Form.Group>
-<Form.Check  className="mb-0  "
-type="checkbox" label="كرة القدم"/>
-<Form.Check  className="mb-0  "
-type="checkbox" label="الكرة الطائرة" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="كرة السلة " />
-<Form.Check  className="mb-0  "
-type="checkbox" label="السباحة " />
-<Form.Check  className="mb-0  "
-type="checkbox" label="ألعاب القوى" />
-
-<Row className="autre">
-  <Col>
-   <Form.Check  className="mb-0  "
-     type="checkbox" label="نوع اخر"/>
-  </Col>
-  <Col sm>
-<Form.Group className="w-50 p-2 mr-3">
-  <Form.Control onChange={e=>setAutresport2(e.target.value)} 
-     type="text" value={autresport2} placeholder="النوع الرياضي الاخر" />
+<Form.Control onChange={e=>setSport4(e.target.value)} 
+type="text" value={sport4} placeholder="اسم الابن او الابنة و عمره (اختيار النوع الرياضي)" />
 </Form.Group>
-  </Col>
-</Row>
+<Form.Select  className="mb-2 mt-2 w-50 p-2" aria-label="Default select example"
+onChange={e=>setSport5(e.target.value)}
+value={sport5}>
+<option>اختر نوع رياضي</option>
+<option value="كرة القدم">كرة القدم </option>
+<option value="الكرة الطائرة">الكرة الطائرة</option>
+<option value="كرة السلة">كرة السلة</option>
+<option value="السباحة">السباحة</option>
+<option value="ألعاب القوى">ألعاب القوى</option>
+<option value="رياضة اخرى"></option>
+</Form.Select>
 
-<Form.Group className="mb-2 mt-2 w-50 p-2">
- <Form.Control onChange={e=>setSport(e.target.value)} 
- type="text" value={sport} placeholder="اسم الابن او الابنة و عمره (اختيار النوع الرياضي)" />
- </Form.Group>
-<Form.Check  className="mb-0  "
-type="checkbox" label="كرة القدم"/>
-<Form.Check  className="mb-0  "
-type="checkbox" label="الكرة الطائرة" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="كرة السلة " />
-<Form.Check  className="mb-0  "
-type="checkbox" label="السباحة " />
-<Form.Check  className="mb-0  "
-type="checkbox" label="ألعاب القوى" />
-
-<Row className="autre">
-  <Col>
-   <Form.Check  className="mb-0  "
-     type="checkbox" label="نوع اخر"/>
-  </Col>
-  <Col sm>
 <Form.Group className="w-50 p-2 mr-3">
-  <Form.Control onChange={e=>setAutresport(e.target.value)} 
-     type="text" value={autresport} placeholder="النوع الرياضي الاخر" />
+ <Form.Control onChange={e=>setAutresport3(e.target.value)} 
+    type="text" value={autresport3} placeholder="النوع الرياضي الاخر" />
 </Form.Group>
-  </Col>
-</Row>
-
-<Form.Group className="mb-2 mt-2 w-50 p-2">
- <Form.Control onChange={e=>setSport(e.target.value)} 
- type="text" value={sport} placeholder="اسم الابن او الابنة و عمره (اختيار النوع الرياضي)" />
- </Form.Group>
-<Form.Check  className="mb-0  "
-type="checkbox" label="كرة القدم"/>
-<Form.Check  className="mb-0  "
-type="checkbox" label="الكرة الطائرة" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="كرة السلة " />
-<Form.Check  className="mb-0  "
-type="checkbox" label="السباحة " />
-<Form.Check  className="mb-0  "
-type="checkbox" label="ألعاب القوى" />
-
-<Row className="autre">
-  <Col>
-   <Form.Check  className="mb-0  "
-     type="checkbox" label="نوع اخر"/>
-  </Col>
-  <Col sm>
-<Form.Group className="w-50 p-2 mr-3">
-  <Form.Control onChange={e=>setAutresport(e.target.value)} 
-     type="text" value={autresport} placeholder="النوع الرياضي الاخر" />
-</Form.Group>
-  </Col>
-</Row>
-
-
-<Form.Group className="mb-2 mt-2 w-50 p-2">
- <Form.Control onChange={e=>setSport(e.target.value)} 
- type="text" value={sport} placeholder="اسم الابن او الابنة و عمره (اختيار النوع الرياضي)" />
- </Form.Group>
-<Form.Check  className="mb-0  "
-type="checkbox" label="كرة القدم"/>
-<Form.Check  className="mb-0  "
-type="checkbox" label="الكرة الطائرة" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="كرة السلة " />
-<Form.Check  className="mb-0  "
-type="checkbox" label="السباحة " />
-<Form.Check  className="mb-0  "
-type="checkbox" label="ألعاب القوى" />
-
-<Row className="autre">
-  <Col>
-   <Form.Check  className="mb-0  "
-     type="checkbox" label="نوع اخر"/>
-  </Col>
-  <Col sm>
-<Form.Group className="w-50 p-2 mr-3">
-  <Form.Control onChange={e=>setAutresport(e.target.value)} 
-     type="text" value={autresport} placeholder="النوع الرياضي الاخر" />
-</Form.Group>
-  </Col>
-</Row>
-
-
+ 
+  
     <div className='sportpreferer'>  <h5>الانشطة الثقافية المفضلة</h5></div>
 
 <Form.Group className="mb-2 mt-2 w-50 p-2">
 <Form.Control onChange={e=>setCulture(e.target.value)} 
 type="text" value={culture} placeholder="اسم الابن او الابنة عمره (اختيار الانشطة الثقافية المفضلة)" />
 </Form.Group>
-<Form.Check  className="mb-0  "
-type="checkbox" label="المسرح" onChange={e=>setTheatre(e.target.value)}
-value={theatre} />
-<Form.Check  className="mb-0  "
-type="checkbox" label="الرسم" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="الموسيقى" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="الرحلات" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="المخيمات" />
 
-<Row className="autre">
-  <Col>
-   <Form.Check  className="mb-0  "
-     type="checkbox" label="نوع اخر"/>
-  </Col>
-  <Col sm>
+<Form.Select  className="mb-2 mt-2 w-50 p-2" aria-label="Default select example"
+ onChange={e=>setCulture2(e.target.value)}
+value={culture2}>    
+   <option>اختر نشاط ثقافي</option>
+   <option value="المسرح">المسرح</option>
+   <option value="الموسيقى">الموسيقى</option>
+   <option value="الرسم">الرسم</option>
+   <option value="الرحلات">الرحلات</option>
+   <option value="المخيمات">المخيمات</option>
+   </Form.Select>
+   
 <Form.Group className="w-50 p-2 mr-3">
   <Form.Control onChange={e=>setAutreculture(e.target.value)} 
      type="text" value={autreculture} placeholder="النشاط الثقافي الاخر"/>
 </Form.Group>
-  </Col>
-</Row>
 
 <Form.Group className="mb-2 mt-2 w-50 p-2">
-<Form.Control onChange={e=>setCulture(e.target.value)} 
-type="text" value={culture} placeholder="اسم الابن او الابنة عمره (اختيار الانشطة الثقافية المفضلة)" />
+<Form.Control onChange={e=>setCulture3(e.target.value)} 
+type="text" value={culture3} placeholder="اسم الابن او الابنة عمره (اختيار الانشطة الثقافية المفضلة)" />
 </Form.Group>
-<Form.Check  className="mb-0  "
-type="checkbox" label="المسرح" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="الرسم" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="الموسيقى" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="الرحلات" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="المخيمات" />
 
-<Row className="autre">
-  <Col>
-   <Form.Check  className="mb-0  "
-     type="checkbox" label="نوع اخر"/>
-  </Col>
-  <Col sm>
+<Form.Select  className="mb-2 mt-2 w-50 p-2" aria-label="Default select example"
+ onChange={e=>setCulture4(e.target.value)}
+value={culture4}>    
+   <option>اختر نشاط ثقافي</option>
+   <option value="المسرح">المسرح</option>
+   <option value="الموسيقى">الموسيقى</option>
+   <option value="الرسم">الرسم</option>
+   <option value="الرحلات">الرحلات</option>
+   <option value="المخيمات">المخيمات</option>
+   </Form.Select>
+   
 <Form.Group className="w-50 p-2 mr-3">
-  <Form.Control onChange={e=>setAutreculture(e.target.value)} 
-     type="text" value={autreculture} placeholder="النشاط الثقافي الاخر"/>
+  <Form.Control onChange={e=>setAutreculture2(e.target.value)} 
+     type="text" value={autreculture2} placeholder="النشاط الثقافي الاخر"/>
 </Form.Group>
-  </Col>
-</Row>
 
 <Form.Group className="mb-2 mt-2 w-50 p-2">
-<Form.Control onChange={e=>setCulture(e.target.value)} 
-type="text" value={culture} placeholder="اسم الابن او الابنة عمره (اختيار الانشطة الثقافية المفضلة)" />
+<Form.Control onChange={e=>setCulture5(e.target.value)} 
+type="text" value={culture5} placeholder="اسم الابن او الابنة عمره (اختيار الانشطة الثقافية المفضلة)" />
 </Form.Group>
-<Form.Check  className="mb-0  "
-type="checkbox" label="المسرح" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="الرسم" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="الموسيقى" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="الرحلات" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="المخيمات" />
 
-<Row className="autre">
-  <Col>
-   <Form.Check  className="mb-0  "
-     type="checkbox" label="نوع اخر"/>
-  </Col>
-  <Col sm>
+<Form.Select  className="mb-2 mt-2 w-50 p-2" aria-label="Default select example"
+ onChange={e=>setCulture6(e.target.value)}
+value={culture6}>    
+   <option>اختر نشاط ثقافي</option>
+   <option value="المسرح">المسرح</option>
+   <option value="الموسيقى">الموسيقى</option>
+   <option value="الرسم">الرسم</option>
+   <option value="الرحلات">الرحلات</option>
+   <option value="المخيمات">المخيمات</option>
+   </Form.Select>
+   
 <Form.Group className="w-50 p-2 mr-3">
-  <Form.Control onChange={e=>setAutreculture(e.target.value)} 
-     type="text" value={autreculture} placeholder="النشاط الثقافي الاخر"/>
+  <Form.Control onChange={e=>setAutreculture3(e.target.value)} 
+     type="text" value={autreculture3} placeholder="النشاط الثقافي الاخر"/>
 </Form.Group>
-  </Col>
-</Row>
-
-<Form.Group className="mb-2 mt-2 w-50 p-2">
-<Form.Control onChange={e=>setCulture(e.target.value)} 
-type="text" value={culture} placeholder="اسم الابن او الابنة عمره (اختيار الانشطة الثقافية المفضلة)" />
-</Form.Group>
-<Form.Check  className="mb-0  "
-type="checkbox" label="المسرح" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="الرسم" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="الموسيقى" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="الرحلات" />
-<Form.Check  className="mb-0  "
-type="checkbox" label="المخيمات" />
-
-<Row className="autre">
-  <Col>
-   <Form.Check  className="mb-0  "
-     type="checkbox" label="نوع اخر"/>
-  </Col>
-  <Col sm>
-<Form.Group className="w-50 p-2 mr-3">
-  <Form.Control onChange={e=>setAutreculture(e.target.value)} 
-     type="text" value={autreculture} placeholder="النشاط الثقافي الاخر"/>
-</Form.Group>
-  </Col>
-</Row>
+ 
  <div className='fichier'>   <h5>يمكن ارسال اية وثيقة في الموضوع في الخانة اسفله</h5></div>
 
   <Form.Group controlId="formFile" className="mb-2 mt-2 w-50 p-2">
